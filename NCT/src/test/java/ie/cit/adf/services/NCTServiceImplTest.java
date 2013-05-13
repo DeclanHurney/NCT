269d8b186b3e17e9dfc3c0aa389216edafbfa5cc
Declan <declanhurney@gmail.com>
2013-05-13 18:38:21 +0100

    more tests + gitignore

--- NCT/src/test/java/ie/cit/adf/services/NCTServiceImplTest.java
+++ NCT/src/test/java/ie/cit/adf/services/NCTServiceImplTest.java
@@ -3,7 +3,9 @@ package ie.cit.adf.services;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import ie.cit.adf.domain.NCTBooking;
+import ie.cit.adf.domain.Vehicle;
 import ie.cit.adf.domain.dao.NCTBookingRepository;
+import ie.cit.adf.domain.dao.VehicleRepository;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,24 +13,34 @@ import org.mockito.Mockito;
 
 public class NCTServiceImplTest {
 
-	private NCTServiceImpl tested;
-	private NCTBookingRepository nctRepository;
-
+	private NCTBookingRepository nctBookingRepository;
+	private NCTServiceImpl nctServiceImpl;
+	private VehicleServiceImpl vehicleServiceImpl;
+	private VehicleRepository vehicleRepository;
+	
 	@Before
-	public void setup() {
-		nctRepository = Mockito.mock(NCTBookingRepository.class);
-		tested = new NCTServiceImpl(nctRepository, null);
+	public void setup(){
+		nctBookingRepository = Mockito.mock(NCTBookingRepository.class);
+		vehicleRepository = Mockito.mock(VehicleRepository.class);
+		nctServiceImpl = new NCTServiceImpl(nctBookingRepository, vehicleRepository);
+		vehicleServiceImpl = new VehicleServiceImpl(vehicleRepository);
 	}
+	
 
 	@Test
 	public void testCreateNCTBooking() {
-		NCTBooking createNewNCTBooking = tested.createNewBooking
+		NCTBooking createNewNCTBooking = nctServiceImpl.createNewBooking
 				("Joe","Bloggs","13D3244","Ford","Mondeo","13-May-2013", "Athlone");
-		Mockito.verify(nctRepository).addNCTBooking(createNewNCTBooking);
+		Mockito.verify(nctBookingRepository).addNCTBooking(createNewNCTBooking);
 		assertThat(createNewNCTBooking.getLocation(), equalTo("Athlone"));
 	}
 
-
+	@Test
+	public void testCreateVehicle() {
+		Vehicle createNewVehicle = vehicleServiceImpl.createNewVehicle("Volkswagen", "beetle", "66D5678");
+		Mockito.verify(vehicleRepository).add(createNewVehicle);
+		assertThat(createNewVehicle.getRegistration(), equalTo("66D5678"));
+	}
 	
 	
 }
